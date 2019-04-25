1988a67f6087c8c2cc604a1ac343fd9e001de1b5
Magiccop990 <49731971+Magiccop990@users.noreply.github.com>
2019-04-25 20:25:40 +0200

    starting over

--- pypet.py
+++ pypet.py
@@ -1,6 +1,5 @@
 
-
-weight = cat.get("weight")
+#those are stats
 cat = {
 "name": "Fluffy",
 "age": 5,
@@ -8,17 +7,15 @@ cat = {
 "hungry": False,
 "photo": '(=^o.o^=)',
 }
-def feed(pet):
-	pet['hungry'] = False 
-	pet['weight'] = pet['weight'] + 1.0
+
+#feeding
+def feed(cat)
+	pet['hungry'] = False
+	pet['weight'] = pet['weight'] + 1.1
+
 feed(cat)
 # now some printing
-
 print("Welcome to Pypet")
 print("Hello " + cat.get("name"))
+print("Am I hungry?" + cat.get("hungry"))
 print(cat.get("photo"))
-
-print(weight)
-
-print(cat.get("hungry"))
-#
