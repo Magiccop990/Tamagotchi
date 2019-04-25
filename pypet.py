827a62587703231305c19ac641c7838987184236
Magiccop990 <49731971+Magiccop990@users.noreply.github.com>
2019-04-25 21:10:28 +0200

    you can feeed it now

--- pypet.py
+++ pypet.py
@@ -1,21 +1,44 @@
+stats
 
-#those are stats
 cat = {
+
 "name": "Fluffy",
+
 "age": 5,
+
 "weight": 9.5,
-"hungry": False,
+
+"hungry": True,
+
 "photo": '(=^o.o^=)',
+
 }
 
-#feeding
-def feed(pet):
-    pet['hungry'] = False
-    pet['weight'] = pet['weight'] + 1.1
 
-feed(cat)
+
+
+
+def feed(pet):
+	if pet['hungry'] == True:
+		pet["hungry"] = False
+		pet['weight'] = pet['weight'] + 1.1
+	else:
+		print("Pypet is not hungry")
+	
+def feeding(pet):
+	if input("Feed your cat? ") == "yes":
+		feed(pet)
+	else:
+		pass
 # now some printing
+
+feeding(cat)
+
 print("Welcome to Pypet")
+
 print("Hello " + cat.get("name"))
-print("Am I hungry?" + cat.get("hungry"))
+
+print("Am I hungry? " + str(cat.get("hungry")))
+
 print(cat.get("photo"))
+print( "Weight " + str(cat.get("weight")))
