e164eec8bce8df961e6608ed57cbcf73df10bced
Magiccop990 <49731971+Magiccop990@users.noreply.github.com>
2019-04-30 07:37:52 +0200

    pypet_0.0.0.1 #30.4.019
    this in my second model of this program but it doesn't work.
    This is the problem - print("Feeding your %s") % (cat.get("name"))
    Could you help me with it?
    Thx.

--- pypet.py
+++ pypet.py
@@ -1,23 +1,34 @@
-stats
+from tkinter import *
+from tkinter import messagebox
+global name, photo
 
+
+name = "cat"
+photo = "(=^o.o^=)"
+
+
+window = Tk()
+window.geometry("1920x1080")
+window.title("PyPet")
+host_nme_lbl = Label(text = "Name:")
+host_pht_lbl = Label(text = "Photo:")
+host_nme_lbl.grid(row = 0, column = 0)
+host_pht_lbl.grid(row = 2, column = 0)
+host = StringVar()
+nme_entry = Entry(window, textvariable = name)
+pht_entry = Entry(window, textvariable = photo)
+nme_entry.grid(row = 1, column = 0)
+pht_entry.grid(row = 3, column = 0)
+#------------------------------------------------------------------------
 cat = {
-
 "name": "Fluffy",
-
 "age": 5,
-
 "weight": 9.5,
-
 "hungry": True,
-
 "photo": '(=^o.o^=)',
-
 }
 
-
-
-
-
+#-----------------------------------------------------------------------
 def feed(pet):
 	if pet['hungry'] == True:
 		pet["hungry"] = False
@@ -28,17 +39,18 @@ def feed(pet):
 def feeding(pet):
 	if input("Feed your cat? ") == "yes":
 		feed(pet)
+		print("Feeding your %s") % (cat.get("name"))
 	else:
-		pass
-# now some printing
+		print("So you want your cat to starve? ")
+#----------------------------------------------------------------------
+
 
 feeding(cat)
 
+
+	
 print("Welcome to Pypet")
-
 print("Hello " + cat.get("name"))
-
 print("Am I hungry? " + str(cat.get("hungry")))
-
 print(cat.get("photo"))
 print( "Weight " + str(cat.get("weight")))
